02aa47d2df97256cc4b6ccc4c9141efca82420d7
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-13 15:59:28 +0700

    added tothost in info

--- hikka/main.py
+++ hikka/main.py
@@ -96,6 +96,7 @@ IS_HIKKAHOST = "HIKKAHOST" in os.environ
 IS_ORACLE = "ORACLE_OS" in os.environ
 IS_AWS = "AWS_OS" in os.environ
 IS_SERV00 = "serv00" in socket.gethostname()
+IS_TOTHOST = "tothost" in socket.gethostname()
 IS_WSL = False
 with contextlib.suppress(Exception):
     from platform import uname
