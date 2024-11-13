49eb0b151db6c803c0c166d0fcd5e5fb177e23e8
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-13 16:14:13 +0700

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -97,6 +97,7 @@ IS_ORACLE = "ORACLE_OS" in os.environ
 IS_AWS = "AWS_OS" in os.environ
 IS_SERV00 = "serv00" in socket.gethostname()
 IS_TOTHOST = "tothost" in socket.gethostname()
+IS_AEZA = "aeza" in socket.gethostname()
 IS_WSL = False
 with contextlib.suppress(Exception):
     from platform import uname
