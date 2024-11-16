1039d392c4f4361c9e0fa9be891a7a86561a33fb
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-16 18:09:58 +0700

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -831,7 +831,7 @@ class Hikka:
 
             await client.hikka_inline.bot.send_animation(
                 logging.getLogger().handlers[0].get_logid_by_client(client.tg_id),
-                "https://x0.at/WO4-.mp4",
+                "https://imgur.com/a/vdOmEWU.png",
                 caption=(
                     "🌘 <b>Hikka {} started!</b>\n\n🌳 <b>GitHub commit SHA: <a"
                     ' href="https://github.com/coddrago/Hikka/commit/{}">{}</a></b>\n✊'
