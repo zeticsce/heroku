7d5cec2aa254c217d8dcbed1cc7e994d7e1598cf
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-19 20:28:49 +0700

    Update test.py

--- hikka/modules/test.py
+++ hikka/modules/test.py
@@ -89,7 +89,7 @@ class TestMod(loader.Module):
                 on_change=self._pass_config_to_logger,
             ),
             loader.ConfigValue(
-                "text",
+                "Text_Of_Ping",
                 "<emoji document_id=5920515922505765329>⚡️</emoji> <b>𝙿𝚒𝚗𝚐: </b><code>{ping}</code><b> 𝚖𝚜 </b>\n<emoji document_id=5900104897885376843>🕓</emoji><b> 𝚄𝚙𝚝𝚒𝚖𝚎: </b><code>{uptime}</code>",
                 lambda: self.strings["configping"],
                 validator=loader.validators.String(),
