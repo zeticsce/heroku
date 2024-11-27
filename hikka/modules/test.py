cd46d869843ec23792ac08ec118b027b18dde377
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-27 23:21:34 +0700

    Update test.py

--- hikka/modules/test.py
+++ hikka/modules/test.py
@@ -104,7 +104,7 @@ class TestMod(loader.Module):
             ),
             loader.ConfigValue(
                 "ping_emoji",
-                🪐,
+                "🪐",
                 lambda: self.strings["ping_emoji"],
                 validator=loader.validators.String(),
             ),
