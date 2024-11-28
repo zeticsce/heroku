a0f3809c63a57433313944813c6b28d6a1dfe6f8
Who? <155328415+coddrago@users.noreply.github.com>
2024-11-28 14:37:45 +0700

    Update test.py

--- hikka/modules/test.py
+++ hikka/modules/test.py
@@ -337,7 +337,7 @@ class TestMod(loader.Module):
             *main.__version__,
             (
                 " <a"
-                f' href="https://github.com/coddrago/Hikka/commit/{ghash}">@{ghash[:8]}</a>'
+                f' href="https://github.com/coddrago/Heroku/commit/{ghash}">@{ghash[:8]}</a>'
                 if ghash
                 else ""
             ),
