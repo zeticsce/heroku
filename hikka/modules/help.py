705485920d72f8097fe19d824eb7e7a146f40b22
Who? <155328415+coddrago@users.noreply.github.com>
2024-12-09 22:08:29 +0700

    Update help.py

--- hikka/modules/help.py
+++ hikka/modules/help.py
@@ -315,7 +315,7 @@ class Help(loader.Module):
 
         await utils.answer(
             message,
-            "{}\n<blockquote>{}</blockquote><blockquote>{}</blockquote>".format(
+            "{}\n <blockquote>{}</blockquote><blockquote>{}</blockquote>".format(
                 reply,
                 "".join(core_ + plain_ + (no_commands_ if force else [])),
                 (
