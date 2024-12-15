7f675c7e7242c76e068b31cb40a5b9eb739f39c9
Who? <155328415+coddrago@users.noreply.github.com>
2024-12-15 20:42:15 +0700

    Please, rename your log chats to heroku-logs, heroku-backups and heroku-assets

--- hikka/database.py
+++ hikka/database.py
@@ -114,10 +114,10 @@ class Database(dict):
         try:
             self._assets, _ = await utils.asset_channel(
                 self._client,
-                "hikka-assets",
-                "🌆 Your Hikka assets will be stored here",
+                "heroku-assets",
+                "🌆 Your Heroku assets will be stored here",
                 archive=True,
-                avatar="https://raw.githubusercontent.com/hikariatama/assets/master/hikka-assets.png",
+                avatar="https://imgur.com/a/TNnJHd0.png"
             )
         except ChannelsTooMuchError:
             self._assets = None
