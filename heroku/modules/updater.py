73bf6488e1094f5246e6159536f7efb8dddcebbc
Who? <155328415+coddrago@users.noreply.github.com>
2025-06-14 09:12:41 +0000

    release

--- heroku/modules/updater.py
+++ heroku/modules/updater.py
@@ -108,7 +108,7 @@ class UpdaterMod(loader.Module):
         if self._pending not in {utils.get_git_hash(), self._notified}:
             m = await self.inline.bot.send_photo(
                 self.tg_id,
-                "https://envs.sh/ONx.png",
+                "https://raw.githubusercontent.com/coddrago/Heroku/refs/heads/master/assets/heroku-update.png",
                 caption=self.strings("update_required").format(
                     utils.get_git_hash()[:6],
                     '<a href="https://github.com/coddrago/Heroku/compare/{}...{}">{}</a>'.format(
